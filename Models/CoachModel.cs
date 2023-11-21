e229cdd3fae4a17ef62c75d4e280430f998062f4
Igor Oliveira <igoroliveira9827@gmail.com>
2023-11-21 18:22:09 -0300

    feature: create user models

---
+++ Models/CoachModel.cs
@@ -0,0 +1,15 @@
+namespace SportsFitPro.Models;
+
+class CoachModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string IDCard { get; set; } = string.Empty;
+    public DateTime Age { get; set; }
+    public List<UserModel>? StudentsList { get; set; }
+    public string specialty { get; set; } = string.Empty;
+
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,13 @@
+namespace SportsFitPro.Models;
+
+class UserModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string? TrainingPreference { get; set; }
+    public DateTime Age { get; set; }
+
+}
