7c9b60960345f40086b5762a234207e0b465ee04
Igor Oliveira <igoroliveira9827@gmail.com>
2023-12-14 16:57:03 -0300

    refactor: refactor user model

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,7 +2,7 @@ namespace SportsFitPro.Models
 {
     public class UserModel 
     {
-        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
         public string  UserName  { get; set; } = string.Empty;
         public string  Email     { get; set; } = string.Empty;
         public string  Password  { get; set; } = string.Empty;
@@ -10,5 +10,6 @@ namespace SportsFitPro.Models
         public string? LastName  { get; set; } = string.Empty;
         public double? Height    { get; set; }
         public double? Weight    { get; set; }
+        public List<WorkOutTrack>? WorkOutTrack { get; set; }
     }
 }
