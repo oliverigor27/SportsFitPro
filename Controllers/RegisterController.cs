31704e95510172cfc0a405ccac58f4a01f732259
Igor Oliveira <igoroliveira9827@gmail.com>
2023-12-14 16:56:08 -0300

    feat: create register controller to handler http requests to  api

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SportsFitPro.Services.Register;
+using SportsFitPro.Models;
+
+namespace SportsFitPro.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RegisterController : ControllerBase 
+{
+    private readonly IRegisterService _registerService;
+
+    public RegisterController(IRegisterService registerService)
+    {
+        _registerService = registerService;        
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserModel>> createUser(UserModel user) {
+
+        await  _registerService.CreateUser(user);
+
+        return Ok( new { Message = "User Created!", user.Id });
+    
+    }
+
+    [HttpPost("{id}")]
+    public async Task<ActionResult> ConfirmPassword(Guid id) {
+
+        var token = _registerService.ConfirmUser(id);
+
+        return Ok(new { Message = $"Usuário {id} cadastrado com sucesso na base de dados!", token });
+    }
+}
